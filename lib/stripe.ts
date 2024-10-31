b5f0bd4b624ca4df0a9296a657681f3de40e13f9
fiston-user <82962030+fiston-user@users.noreply.github.com>
2024-10-31 14:11:41 +0100

    correct stripe api version

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,10 +1,12 @@
-import Stripe from 'stripe';
+import Stripe from "stripe";
 
 if (!process.env.STRIPE_SECRET_KEY) {
-  throw new Error('STRIPE_SECRET_KEY is missing. Please add it to your .env file');
+  throw new Error(
+    "STRIPE_SECRET_KEY is missing. Please add it to your .env file"
+  );
 }
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2023-10-16',
+  apiVersion: "2023-08-16",
   typescript: true,
 });
