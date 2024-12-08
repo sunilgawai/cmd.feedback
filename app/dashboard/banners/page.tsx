cf65a9cc9e3738c6d55b704417e673f7bca04909
sunilgawai <sunilgpivotech@gmail.com>
2024-12-09 02:36:00 +0530

    banners page added

--- app/dashboard/banners/page.tsx
+++ app/dashboard/banners/page.tsx
@@ -1,5 +1,4 @@
-import { auth } from "@/app/auth";
-import { redirect } from "next/navigation";
+"use client";
 import {
   Card,
   CardContent,
@@ -7,38 +6,105 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import Autoplay from "embla-carousel-autoplay";
 import { TeamList, TeamListSkeleton } from "@/components/teams/team-list";
-import { Suspense } from "react";
+import { Suspense, useState, useEffect } from "react";
 import { CreateDialog } from "@/components/create-dialog";
 import NotificationForm from "@/components/forms/notification-form";
+import { FileUploadExample } from "@/components/examples/file-upload-example";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+  type CarouselApi,
+} from "@/components/ui/carousel";
 
-export default async function BannersPage() {
-  const session = await auth();
+export default function BannersPage() {
+  const [api, setApi] = useState<CarouselApi>();
+  const [current, setCurrent] = useState(0);
+  const [count, setCount] = useState(0);
 
-  if (!session?.user) {
-    redirect("/login");
-  }
+  useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    setCount(api.scrollSnapList().length);
+    setCurrent(api.selectedScrollSnap() + 1);
+
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap() + 1);
+    });
+  }, [api]);
 
   return (
     <div className="flex flex-col gap-8">
       <div className="flex justify-between items-center">
-        <h1 className="text-3xl font-bold">Offers</h1>
+        <h1 className="text-3xl font-bold">Your Banners</h1>
         <CreateDialog
           title="Create Notification"
           description="Create your notification here"
-          form={<NotificationForm />}
+          form={NotificationForm}
         />
       </div>
-      <Card>
-        <CardHeader>
-          <CardTitle>Your Notification</CardTitle>
-          <CardDescription>Notification you created.</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Suspense fallback={<TeamListSkeleton />}>
-            {/* <TeamList teams={teams} /> */}
-          </Suspense>
-        </CardContent>
+      {/* File Upload */}
+      <div className="flex flex-col gap-4">
+        <div>
+          <h3 className="font-semibold">FileUpload</h3>
+          <p className="text-sm text-muted-foreground mb-4">
+            Drag and drop file upload component with preview
+          </p>
+          <FileUploadExample />
+        </div>
+      </div>
+      <div className="flex items-center justify-center text-2xl font-semibold">
+        <h1>Current Banner</h1>
+      </div>
+      <Card className="w-full grid place-content-center gap-4 py-4">
+        <Carousel
+          plugins={[
+            Autoplay({
+              delay: 2000,
+            }),
+          ]}
+          setApi={setApi}
+          className="w-full max-w-2xl"
+        >
+          <CarouselContent className="-ml-1">
+            {/* {Array.from({ length: 5 }).map((_, index) => (
+              <CarouselItem key={index}>
+                <Card>
+                  <CardContent className="flex aspect-square items-center justify-center p-6">
+                    <span className="text-4xl font-semibold">{index + 1}</span>
+                  </CardContent>
+                </Card>
+              </CarouselItem>
+            ))} */}
+            {Array.from({ length: 10 }).map((_, index) => (
+              <CarouselItem
+                key={index}
+                className="pl-1 md:basis-1/2 lg:basis-1/3"
+              >
+                <div className="p-1">
+                  <Card>
+                    <CardContent className="flex aspect-square items-center justify-center p-6">
+                      <span className="text-2xl font-semibold">
+                        {index + 1}
+                      </span>
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+        <div className="py-2 text-center text-sm text-muted-foreground">
+          Slide {current} of {count}
+        </div>
       </Card>
     </div>
   );
