e697794756c9094a6b2a135c200e53995516bde6
sunilgawai <sunilgpivotech@gmail.com>
2024-12-09 02:46:43 +0530

    removed unused code

--- app/dashboard/actions/index.ts
+++
@@ -1,9 +0,0 @@
-"use server";
-
-import { prisma } from "@/lib/prisma";
-
-export const getDashboardStats = async () => {
-  const agents = await prisma.agent.findMany();
-
-  return { agents };
-};
