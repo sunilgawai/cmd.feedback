c3a1701aed94eb683072f86788564898607d8687
sunilgawai <sunilgawai7397@gmail.com>
2024-12-10 12:33:28 +0530

    fuck

--- components/data-table/data-table-search.tsx
+++ components/data-table/data-table-search.tsx
@@ -10,11 +10,11 @@ interface DataTableSearchProps {
   searchQuery: string;
   setSearchQuery: (
     value: string | ((old: string) => string | null) | null,
-    options?: Options<any> | undefined
+    options?: any | undefined
   ) => Promise<URLSearchParams>;
   setPage: <Shallow>(
     value: number | ((old: number) => number | null) | null,
-    options?: Options<Shallow> | undefined
+    options?: any
   ) => Promise<URLSearchParams>;
 }
 
