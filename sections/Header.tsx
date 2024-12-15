20034184f6be4a614cc70a35cfee3f0748877310
sunilgawai <sunilgawai7397@gmail.com>
2024-12-15 21:17:21 +0530

    logo:yellow added

--- sections/Header.tsx
+++ sections/Header.tsx
@@ -13,7 +13,7 @@ export default function Header() {
               height={80}
               width={100}
               className="w-32 md:w-32 h-auto my-4"
-              src="/logo_white.png"
+              src="/logo_yellow.png"
               alt="logo"
               priority
             />
